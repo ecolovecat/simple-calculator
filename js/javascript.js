e7701247f132adc31c4f27b7265eddf823877bac
ecolovecat <tranphuongduy0403@gmail.com>
2021-10-24 22:48:31 +0700

    push

---
+++ js/javascript.js
@@ -0,0 +1,90 @@
+/**
+ * Còn
+ * Xử lý nhập số âm ( hiện số âm khi ấn dấu trừ đầu tiên)
+ * Xử lý màu khi ấn nút cộng trừ nhân chia
+ */
+
+var print = '';
+var cal = '';
+var oldMath = '';
+var newMath = '';
+
+function set_num(num) {
+
+    print += num;
+    document.getElementById('kq').value = print;
+    cal += num;
+
+}
+
+
+
+function set_math(math) {
+    // document.getElementById(cal[cal.length - 1]).classList.remove("zoom");
+    if (cal[cal.length - 1] != "+" && cal[cal.length - 1] != "-" && cal[cal.length - 1] != "*" && cal[cal.length - 1] != "/" && cal[cal.length - 1] != "%") {
+        cal += math;
+        console.log("true");
+    } else {
+        oldMath = cal[cal.length - 1];
+        document.getElementById(oldMath).classList.remove("zoom");
+        cal = cal.slice(0, -1);
+        cal += math;
+    }
+    newMath = cal[cal.length - 1];
+    document.getElementById(cal[cal.length - 1]).classList.add("zoom");
+    oldMath = newMath;
+    console.log(cal[cal.length - 1])
+
+    print = '';
+
+    // console.log(cal);
+    // console.log(print);
+}
+
+var oldCal = cal;
+
+function delete_each() {
+
+    print = print.slice(0, -1);
+
+    if (print.length > 0) {
+        cal = cal.slice(0, -1);
+        document.getElementById('kq').value = print;
+
+    } else {
+        cal = oldCal; // xử lý sô đầu không xóa được
+    }
+    document.getElementById('kq').value = print;
+
+
+    // console.log(cal);
+    // console.log(print);
+
+}
+
+function delete_all() {
+    // ls = document.getElementsByClassName("system");
+    // for (i = 0; i < ls.length; i++) {
+    //     if (ls[i] == document.getElementsByClassName("zoom")) {
+    //         ls[i].classList.remove("zoom");
+
+    //     }
+    // }
+
+    print = '';
+    cal = '';
+    document.getElementById('kq').value = print;
+    // console.log(cal);
+    // console.log(print);
+    // x = document.getElementById(newMath).classList.remove("zoom");
+}
+
+function set_result() {
+    document.getElementById('kq').value = eval(cal);
+    document.getElementById(newMath).classList.remove("zoom");
+    oldMath = newMath;
+    print = '';
+    cal = '';
+    // console.log(cal);
+    // console.log(print);
+}
